44c059459b520cfafb72c88965d57fd8d8b95cad
victornjeru49 <victornjeru49@gmail.com>
2025-05-16 18:57:51 +0300

    Refactor product rendering and cart display; add functionality for adding items to cart

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,29 @@ console.log(data)
   console.log('Database Sucessfull')
   await displayProducts()
 
-  function renderProducts(products: ProductCart[], card: HTMLDivElement) {
+  async function renderProducts(products: ProductCart[], card: HTMLDivElement) {
 
     card.innerHTML = products.map((product: ProductCart) => `
         <div class="products-card" id="product-${product.id}">
             <div class="products-content">
                 <img src="${product.image.desktop}" alt="${product.name}" class="product-thumbnail" />
+                <button onclick ="addItems(${product.id})"> 
+                <img src="../public/assets/images/icon-add-to-cart.svg" alt="">
+                add to cart
+                </button>
                 <h3>${product.name}</h3>
                 <p>Category: ${product.category}</p>
-                <p>Price: $${product.price.toFixed(2)}</p>
+                <p>Price: $${product.price.toFixed(2)}</p>     
             </div>
         </div>
     `).join('');
+}
+
+
+async function addItems() {
+  
+
+
 }
 
 async function displayProducts() {
@@ -47,9 +58,32 @@ async function displayProducts() {
     }
 }
 
+async function addCart():Promise<void> {
+  const card = document.querySelector<HTMLDivElement>('.cart-content')!; 
 
 
-// Call the function to display products
+
+  if (card && card.innerHTML.trim() === '') {
+    card.innerHTML = `
+      <div class="emptyclass">
+        <img src="../public/assets/images/illustration-empty-cart.svg" alt="">
+        <p> your added items will appear here</p>
+      </div>
+    `;
+  } else if (card) {
+    card.innerHTML = `
+      <div class ="">
+        <div class="cart-section">
+        </div>
+        <h2> Total Order: </h2>
+        <button> Confirm Order </button>
+      </div>
+    `;
+  }
+}
+
+addCart()
+
 displayProducts();
 
  }
@@ -60,6 +94,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
 
     <div class ="cart">
+    <h1> Your Cart (0)</h1>
+        <div class ="cart-content">
+        </div>
     </div>
 
   </div>
